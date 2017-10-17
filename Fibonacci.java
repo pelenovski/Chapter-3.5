90901751312b695e80f7e53e096be572efe2bdc1
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-10-17 09:31:13 -0500

    Fibonacci Program

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+//Pane Elenovski
+//10/17/2017
+//Fibonacci Sequence
+//Runs the Fibonacci sequence until a value inputted by user is met.
+
+import java.util.Scanner;
+
+public class Fibonacci
+{
+	public static void main(String []args)
+	{
+		int limit, fib;
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Enter a limit for the sequence to run up to. ");
+		limit = keyboard.nextInt();
+
+		for
+
+	}
+}
