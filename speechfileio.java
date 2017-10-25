141d5ed878870f21d998298bb0b5fb8a81dea1ba
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-10-25 08:56:07 -0500

    speech

---
+++ speechfileio.java
@@ -0,0 +1,20 @@
+//Pane Elenovski
+//10/25/2017
+//Speech File IO
+//Takes a file using file IO and counts number of certain words within different speeches
+
+import java.io.*;
+import java.util.Scanner;
+
+public class speechfileio
+{
+	public static void main (String[] args)
+	{
+
+
+
+
+
+
+	}
+}
