d764979caec5900020649a2c799a0e94e187af01
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-10-27 09:32:08 -0500

    drunkwalk program

---
+++ drunkwalk.java
@@ -0,0 +1,24 @@
+//Pane Elenovski
+//10/27/2017
+//Drunk Walk
+//Randomly walk in different directions
+
+import java.util.Random;
+import TurtleGraphics.StandardPen;
+import java.awt.*;
+
+public class drunkwalk
+{
+	public static void main(String[] args)
+	{
+		Random generator = new Random();
+
+		int m = generator.nextInt(5)+1
+
+
+		for
+
+
+
+	}
+}
