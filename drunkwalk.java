5a9e7466bc2f83b1f418c79e892acb537e54b46c
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-11-02 07:34:29 -0500

    drunk walk

--- drunkwalk.java
+++ drunkwalk.java
@@ -11,27 +11,38 @@ public class drunkwalk
 {
 	public static void main(String[] args)
 	{
+		StandardPen p = new StandardPen();
 		Random generator = new Random();
-		pen.up();
-		int m;
+		p.down();
+		int m = 0;
 
-		for (x = 0; x <= 250; x++)
+		for (int x = 0; x <= 250; x++)
+		{
 			//Generates random num in between 1 and 4 inclusive
-			m = generator.nextInt(5)+1;
+			m = generator.nextInt(4)+1;
 
 			//turns pen 90 degrees and writes for 10 blocks
 			if (m == 1)
-				pen.turn(90);
-				pen.move(10);
-			if (m == 2)
-				pen.turn(180);
-				pen.move(10);
-			if (m == 3)
-				pen.turn(270);
-				pen.move(10);
-			if (m == 4)
-				pen.turn(360);
-				pen.move(10);
+			{
+				p.turn(90);
+				p.move(10);
+			}
+			else if (m == 2)
+			{
+				p.turn(180);
+				p.move(10);
+			}
+			else if (m == 3)
+			{
+				p.turn(270);
+				p.move(10);
+			}
+			else if (m == 4)
+			{
+				p.turn(360);
+				p.move(10);
+			}
+		}
 
 	}
 }
